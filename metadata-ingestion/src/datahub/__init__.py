d526e22ee2a6e6007da0755128dac70ffcb17805
Harshal Sheth <hsheth2@gmail.com>
2025-01-23 09:17:06 -0800

    start defining public interface

--- metadata-ingestion/src/datahub/__init__.py
+++ metadata-ingestion/src/datahub/__init__.py
@@ -1,6 +1,27 @@
 import sys
 import warnings
 
+import datahub.metadata.schema_classes as models
+from datahub.errors import SdkUsageError
+from datahub.ingestion.graph.client import DataHubGraph, get_default_graph
+from datahub.ingestion.graph.config import DatahubClientConfig
+from datahub.metadata.urns import (
+    ChartUrn,
+    ContainerUrn,
+    CorpGroupUrn,
+    CorpUserUrn,
+    DashboardUrn,
+    DataPlatformInstanceUrn,
+    DataPlatformUrn,
+    DatasetUrn,
+    DomainUrn,
+    GlossaryTermUrn,
+    SchemaFieldUrn,
+    TagUrn,
+)
+from datahub.sdk.container import Container
+from datahub.sdk.dataset import Dataset
+
 # Published at https://pypi.org/project/acryl-datahub/.
 __package_name__ = "acryl-datahub"
 __version__ = "1!0.0.0.dev0"
