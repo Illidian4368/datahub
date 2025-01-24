8dba29033185bd62675c0ef2693ca6db28980379
illidian4368 <illidian4368@gmail.com>
2025-01-24 11:32:59 +0100

    Create Untitled-2.py

---
+++ buildSrc/Untitled-2.py
@@ -0,0 +1,16 @@
+# TODO: place your cursor at the end of line 5 and press Enter to generate a suggestion.
+# Tip: press tab to accept the suggestion
+
+    fake_users = [
+            { "name": "User 1", "id": "user1", "city": "San Francisco", "state": "CA" },
+            { "name": "User 2", "id": "user2", "city": "New York", "state": "NY" },
+            { "name": "User 3", "id": "user3", "city": "Los Angeles", "state": "CA" },
+            { "name": "User 4", "id": "user4", "city": "Chicago", "state": "IL" },
+            { "name": "User 5", "id": "user5", "city": "Houston", "state": "TX" },
+            { "name": "User 6", "id": "user6", "city": "Phoenix", "state": "AZ" },
+            { "name": "User 7", "id": "user7", "city": "Philadelphia", "state": "PA" },
+            { "name": "User 8", "id": "user8", "city": "San Antonio", "state": "TX" },
+            { "name": "User 9", "id": "user9", "city": "San Diego", "state": "CA" },
+            { "name": "User 10", "id": "user10", "city": "Dallas", "state": "TX" },
+    ]
+        print(fake_users)
